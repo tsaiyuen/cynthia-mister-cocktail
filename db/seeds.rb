cf2d71ed22145795f14004e59b4dab2c40cd40d9
Cynthia Tsai <tsaiyuen@gmail.com>
2020-11-20 13:03:14 -0300

    Mr cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning ingredients..."
+Ingredient.destroy_all
+
+puts "Creating ingredients..."
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+puts "Finished!"
